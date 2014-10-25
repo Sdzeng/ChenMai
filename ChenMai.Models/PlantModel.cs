49a5e4460dc072acec703c1688e251d4623c4462
Sdzeng <zsongdong@163.com>
2014-10-25 08:43:54 +0800

    add model object

---
+++ ChenMai.Models/PlantModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChenMai.Models
+{
+    public class PlantModel
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Origin { get; set; }
+
+        public string Remark { get; set; }
+
+
+        public string CreateUser { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string ModifyUser { get; set; }
+
+        public Nullable<DateTime> ModifyDate { get; set; }
+
+    }
+}
